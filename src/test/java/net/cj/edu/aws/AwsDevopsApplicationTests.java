51e983a28623bf1cf8a9cde441ef3b69090ee104
leeyoungtaek <lyt15@cj.net>
2022-11-11 14:20:18 +0900

    첫번째 커밋

---
+++ src/test/java/net/cj/edu/aws/AwsDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cj.edu.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
